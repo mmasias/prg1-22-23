8bae1ad882334f5b07d45d1ad3714826efd2ef7c
Hexcss <98705767+Hexcss@users.noreply.github.com>
2022-10-18 23:15:49 +0200

    Area Calculator Challenge

---
+++ retos/entregas/javierCader/areaCalculator/src/App.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+import classes.Calculator;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        boolean run = true;
+
+        while (run) {
+
+            new Calculator();
+
+            int continueRunning = JOptionPane.showConfirmDialog(null, "Do you want to do another operation?",
+                    "Continue Running", JOptionPane.YES_NO_OPTION);
+
+            if (continueRunning == 1) {
+                run = false;
+            }
+        }
+    }
+}
