d470b703c971d9c8452da4914e0ad48350356abe
RaulPlayo <114569807+RaulPlayo@users.noreply.github.com>
2022-10-19 13:17:47 +0200

    Create VariablesAreas.java

---
+++ retos/entregas/raulPelayo./VariablesAreas.java
@@ -0,0 +1,27 @@
+public class Areas {
+    public static void main (String[]args) {
+            
+            int AreaDeCuadrado;  
+            AreaDeCuadrado = 2 * 2;
+            System.out.println("El lado del cuadrado mide 2");
+            System.out.println("Si el lado del cuadrado mide 2, el area es" +  AreaDeCuadrado);
+
+            int AreaDeRectangulo;
+            AreaDeRectangulo = 5 * 3;
+            System.out.println("La base del rectangulo mide 5 y la altura 3");
+            System.out.println("Si la base mide 5 y la altura 3, el area es" +  AreaDeRectangulo);
+            
+            double Pi;
+            Pi = 3.141592;
+            double AreaDeCirculo;
+            AreaDeCirculo = Pi * 4 * 4;
+            System.out.println("El area del circulo es Pi x el radio al cuadrado, el radio es 4 y Pi=3,141592");
+            System.out.println("Si el radio es 4, el area del circulo es" +  AreaDeCirculo);
+
+            
+            
+            
+        
+            
+     }
+}
