470ef949a8c01a4a0f5467d1edc360bba93e9dd2
AndrextA <andrextalarcon@gmail.com>
2022-10-19 16:52:13 +0200

    CalculadoraNotaFinal

---
+++ retos/entregas/andrésAlarcón/Calculadora nota final/CalculadoraNotaFinal.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CalculadoraNotaFinal {
+    public static void main(String[] args) {
+        
+        Scanner lector = new Scanner(System.in);
+
+        double examen_final, examen_parcial, evaluacion_continua, evaluacion_profesor, nota_final;
+
+        System.out.println("Ingrese la nota del examen final");
+        examen_final = lector.nextDouble();
+        System.out.println("Ingrese la nota del examen parcial");
+        examen_parcial = lector.nextDouble();
+        System.out.println("Ingrese la nota de la evaluación continua");
+        evaluacion_continua = lector.nextDouble();
+        System.out.println("Ingrese la nota de la evaluación del profesor");
+        evaluacion_profesor = lector.nextDouble();
+
+        nota_final = examen_final * 0.5 + examen_parcial * 0.25 + evaluacion_continua * 0.2 + evaluacion_profesor * 0.05;
+
+        System.out.println("La nota final es: "+nota_final);
+
+        lector.close();
+    }
+}
