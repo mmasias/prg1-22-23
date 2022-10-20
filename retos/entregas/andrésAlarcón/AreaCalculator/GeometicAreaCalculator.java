c3ea9c643f252fcd483974fc3d2970eebee8b502
AndrextA <andrextalarcon@gmail.com>
2022-10-20 23:32:33 +0200

    Update

--- retos/entregas/andrésAlarcón/AreaCalculator/GeometicAreaCalculator.java
+++ retos/entregas/andrésAlarcón/AreaCalculator/GeometicAreaCalculator.java
@@ -3,15 +3,47 @@ import java.util.Scanner;
 public class GeometicAreaCalculator {
     public static void main(String[] args) {
 
-        double side, base, heigth, ratio, pi;
-
         Scanner sc=new Scanner(System.in);
-        System.out.println("");
 
+        System.out.println("Select the figure you want to know the area: 1. Square 2.Rectangle 3. Triangle 4. Circle");
 
+        int option;
+        option = sc.nextInt();
+        switch (option) {
+            case 1:
+                System.out.println("Enter the base");
+                double basesquare = sc.nextDouble();
+                double Square = basesquare * basesquare;
+                System.out.println("The area is " + Square);
+                break;
 
+            case 2:
+                System.out.println("Enter the base");
+                double baserectangle = sc.nextDouble();
+                System.out.println("Enter the heigth");
+                double heigthrectangle = sc.nextDouble();
+                double Rectangle = baserectangle * heigthrectangle;
+                System.out.println("The area is " + Rectangle);
+                break;
 
+            case 3:
+                System.out.println("Enter the base");              
+                double basetriangle = sc.nextDouble();
+                System.out.println("Enter the heigth");
+                double heigthtriangle = sc.nextDouble();
+                double Triangle = (basetriangle * heigthtriangle)/2;
+                System.out.println("The area is " + Triangle);
+                break;
 
-
+            case 4:
+                System.out.println("Enter the radius");
+                double radius = sc.nextDouble();
+                double Circle = Math.PI * Math.pow(radius, 2);
+                System.out.println("The area is " + Circle);
+                break;
+                
+        };
+        
+        sc.close();
     }
 }
