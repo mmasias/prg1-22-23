20cc48c8115c7b59e4e522f73331bf5f971b5458
DavidGarciaCosta <114480378+DavidGarciaCosta@users.noreply.github.com>
2022-10-20 11:00:36 +0200

    Add files via upload

---
+++ retos/entregas/davidGarcía/Miniretos1Ascii.java
@@ -0,0 +1,61 @@
+public class Miniretos1Ascii {
+    public static void main(String[]args) {
+        System.out.println ("**********");
+        System.out.println ("**********");
+        System.out.println ("**********");
+        System.out.println ("**********");
+        System.out.println ("**********");
+
+        System.out.println(" ");
+        System.out.println(" **  **  **");
+        System.out.println(" **  **  **");
+        System.out.println("   **  ** ");
+        System.out.println("   **  ** ");
+        System.out.println(" **  **  **");
+        System.out.println(" **  **  **");
+       
+       
+        System.out.println(" ");
+        System.out.println("+--------------------+");
+        System.out.println("|**  **  **  **  **  |");
+        System.out.println("|**  **  **  **  **  |");
+        System.out.println("|  **  **  **  **  **|");
+        System.out.println("|  **  **  **  **  **|");
+        System.out.println("|**  **  **  **  **  |");
+        System.out.println("|**  **  **  **  **  |");
+        System.out.println("|  **  **  **  **  **|");
+        System.out.println("|  **  **  **  **  **|");
+        System.out.println("+--------------------+ ");
+
+        System.out.println("      *");
+        System.out.println("     ***");
+        System.out.println("    *****");
+        System.out.println("   *******");
+        System.out.println("  ********");
+        System.out.println("  *********");
+        System.out.println("  *********");
+        System.out.println("  *********");
+        System.out.println("  *********");
+        System.out.println("  *********");
+
+        System.out.println("      *");
+        System.out.println("     ***");
+        System.out.println("    *****");
+        System.out.println("   *******");
+        System.out.println("  ********");
+        System.out.println("  *********");
+        System.out.println("  *********");
+        System.out.println("  ***   ***");
+        System.out.println("  ***   ***");
+        System.out.println("  *********");
+
+
+
+
+
+
+
+
+        
+    }
+}
