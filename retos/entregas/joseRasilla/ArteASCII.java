7337b9c8f415480b9e64133fcd99847d8f771fe9
joserasilla-uneat <jose.rasilla@alumnos.uneatlantico.es>
2022-10-25 13:13:43 +0200

    Finalizado ArteASCII

--- retos/entregas/joseRasilla/ArteASCII.java
+++ retos/entregas/joseRasilla/ArteASCII.java
@@ -1,64 +1,67 @@
 public class ArteASCII {
     public static void main(String[]args){
+        
         System.out.println("Reto 1:");
-        System.out.println("\n");
+        System.out.println("");
         System.out.println("**********");
         System.out.println("**********");
         System.out.println("**********");
         System.out.println("**********");
         System.out.println("**********");
-        System.out.println("\n");
-        System.out.println("Reto :");
-        System.out.println("\n");
-        System.out.println("**  **  **");
-        System.out.println("**  **  **");
-        System.out.println("  **  **  ");
-        System.out.println("  **  **  ");
-        System.out.println("**  **  **");
-        System.out.println("**  **  **");
-        System.out.println("\n");
-        System.out.println("Reto :");
-        System.out.println("\n");
-        System.out.println("+--------------------+");
-        System.out.println("|**  **  **  **  **  |");
-        System.out.println("|**  **  **  **  **  |");
-        System.out.println("|  **  **  **  **  **|");
-        System.out.println("|  **  **  **  **  **|");
-        System.out.println("|**  **  **  **  **  |");
-        System.out.println("|**  **  **  **  **  |");
-        System.out.println("|  **  **  **  **  **|");
-        System.out.println("|  **  **  **  **  **|");
-        System.out.println("+--------------------+");
-        System.out.println("\n");
-        System.out.println("Reto :");
-        System.out.println("\n");
-        System.out.println("    *");
-        System.out.println("   ***");
-        System.out.println("  *****");
-        System.out.println(" *******");
-        System.out.println("********");
-        System.out.println("*********");
-        System.out.println("*********");
-        System.out.println("*********");
-        System.out.println("*********");
-        System.out.println("*********");
-        System.out.println("\n");
-        System.out.println("Reto :");
-        System.out.println("\n");
-        System.out.println("    *");
-        System.out.println("   ***");
-        System.out.println("  *****");
-        System.out.println(" *******");
-        System.out.println("********");
-        System.out.println("*********");
-        System.out.println("*********");
-        System.out.println("*********");
-        System.out.println("***   ***");
-        System.out.println("***   ***");
-        System.out.println("*********");
-        System.out.println("\n");
-        System.out.println("Reto :");
-        System.out.println("\n");
-    }
+        System.out.println("");
 
+        System.out.println("Reto 2:");
+        System.out.println("");
+        System.out.println("**  **  **");
+        System.out.println("**  **  **");
+        System.out.println("  **  **  ");
+        System.out.println("  **  **  ");
+        System.out.println("**  **  **");
+        System.out.println("**  **  **");
+        System.out.println("");
+
+        System.out.println("Reto 3:");
+        System.out.println("");
+        System.out.println("+--------------------+");
+        System.out.println("|**  **  **  **  **  |");
+        System.out.println("|**  **  **  **  **  |");
+        System.out.println("|  **  **  **  **  **|");
+        System.out.println("|  **  **  **  **  **|");
+        System.out.println("|**  **  **  **  **  |");
+        System.out.println("|**  **  **  **  **  |");
+        System.out.println("|  **  **  **  **  **|");
+        System.out.println("|  **  **  **  **  **|");
+        System.out.println("+--------------------+");
+        System.out.println("");
+
+        System.out.println("Reto 4:");
+        System.out.println("");
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("");
+
+        System.out.println("Reto 5:");
+        System.out.println("");
+        System.out.println("    *");
+        System.out.println("   ***");
+        System.out.println("  *****");
+        System.out.println(" *******");
+        System.out.println("********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("*********");
+        System.out.println("***   ***");
+        System.out.println("***   ***");
+        System.out.println("*********");
+        System.out.println("");
+
+    }
 }
