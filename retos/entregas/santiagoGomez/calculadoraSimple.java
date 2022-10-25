e02d8cd9044e4bca6e2ca9247b59b708ac179a22
Santiago Gómez de la Torre <102590833+SantiGT1503@users.noreply.github.com>
2022-10-25 12:28:58 +0200

    Update calculadoraSimple.java

--- retos/entregas/santiagoGomez/calculadoraSimple.java
+++ retos/entregas/santiagoGomez/calculadoraSimple.java
@@ -1,7 +1,7 @@
 package intervencionUsuario;
 import java.util.Scanner;
 
-public class calculadora{
+public class calculadoraSimple{
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
