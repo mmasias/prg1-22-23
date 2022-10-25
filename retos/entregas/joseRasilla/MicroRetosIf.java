7e8b1d37fe37b1f3ac430f037827c1d89c2f051e
joserasilla-uneat <114484500+joserasilla-uneat@users.noreply.github.com>
2022-10-26 00:27:06 +0200

    Update and rename microRetosIf.java to MicroRetosIf.java

--- retos/entregas/joseRasilla/MicroRetosIf.java
+++ retos/entregas/joseRasilla/MicroRetosIf.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class microRetosIf {
+public class MicroRetosIf {
     public static void main(String[] args) {
 
         Scanner entrada = new Scanner(System.in);
