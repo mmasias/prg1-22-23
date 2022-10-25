a128e7b6ac5f308d86f9875f76968adba5247d1c
joserasilla-uneat <114484500+joserasilla-uneat@users.noreply.github.com>
2022-10-26 00:25:26 +0200

    Update and rename adivinacion.java to Adivinacion.java

--- retos/entregas/joseRasilla/Adivinacion.java
+++ retos/entregas/joseRasilla/Adivinacion.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class adivinacion {
+public class Adivinacion {
     public static void main(String[] args) {
         
         Scanner entrada = new Scanner(System.in);
