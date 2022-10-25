f076d47fe13844c1c333dd4e9fae24c4e3db6c7c
DavidGarciaCosta <114480378+DavidGarciaCosta@users.noreply.github.com>
2022-10-25 12:28:36 +0200

    Update Miniretos1Asciidavid.java

--- retos/entregas/davidGarcía/Miniretos1Asciidavid.java
+++ retos/entregas/davidGarcía/Miniretos1Asciidavid.java
@@ -1,4 +1,4 @@
-public class Miniretos1Ascii {
+public class Miniretos1Asciidavid {
     public static void main(String[]args) {
         System.out.println ("**********");
         System.out.println ("**********");
