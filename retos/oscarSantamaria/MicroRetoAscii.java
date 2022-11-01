a4e18f4ed7e6eb69908384987508cd2abc6a0cf0
Oscar Santamaria <114479802+Oscarsantasanchez@users.noreply.github.com>
2022-11-01 19:52:51 +0100

    Add files via upload

---
+++ retos/oscarSantamaria/MicroRetoAscii.java
@@ -0,0 +1,32 @@
+public class MicroRetoAscii {
+    public static void main(String[] args){
+        
+        System.out.println("Figura uno:");
+		System.out.println(" **********");
+		System.out.println(" **********");
+		System.out.println(" **********");
+		System.out.println(" **********");
+		System.out.println(" **********");
+		System.out.println("\n");
+        System.out.println("Figura dos:");
+        System.out.println(" **  **  **");
+		System.out.println(" **  **  **");
+		System.out.println("   **  **  ");
+		System.out.println("   **  **  ");
+		System.out.println(" **  **  **");	
+        System.out.println(" **  **  **");	
+        System.out.println("\n");
+        System.out.println("Figura tres:");
+        System.out.println("     *    ");
+		System.out.println("    ***   ");
+		System.out.println("   *****  ");
+		System.out.println("  ******* ");
+		System.out.println(" *********");	
+        System.out.println(" *********");
+        System.out.println(" *********");
+        System.out.println(" *********");
+        System.out.println(" *********");
+        System.out.println(" *********");
+
+    }
+}
---
+++ retos/oscarSantamaria/UnDibujo.java
@@ -0,0 +1,17 @@
+public class UnDibujo {
+    public static void main(String[] args){
+
+		System.out.println("+--------------------+");
+		System.out.println("|**  **  **  **  **  |");
+		System.out.println("|**  **  **  **  **  |");
+		System.out.println("|  **  **  **  **  **|");
+		System.out.println("|  **  **  **  **  **|");
+		System.out.println("|**  **  **  **  **  |");
+		System.out.println("|**  **  **  **  **  |");
+		System.out.println("|  **  **  **  **  **|");
+        System.out.println("|  **  **  **  **  **|");
+		System.out.println("|  **  **  **  **  **|");
+		System.out.println("+--------------------+");		
+
+    }
+}
