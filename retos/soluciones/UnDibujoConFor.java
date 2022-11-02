c3a12b1ef5b115ecabe711f595dfc66bd2555b68
mmasias@AIO <manuel@masiasweb.com>
2022-11-02 12:10:11 +0100

    Proceso de creación...

---
+++ retos/soluciones/UnDibujoConFor.java
@@ -0,0 +1,12 @@
+class UnDibujoConFor {
+    public static void main(String[] args){
+
+        for(int i=0;i<5;i=i+1){
+            for(int j=0;j<9;j=j+1){
+                System.out.print("*");    
+            }
+            System.out.println();
+        }
+
+    }
+}
