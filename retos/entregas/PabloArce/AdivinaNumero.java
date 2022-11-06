d8073bc854d44c3e9b8b8d60cd50712f635236b9
pablarce <113457050+pablarce@users.noreply.github.com>
2022-11-06 20:20:36 +0100

    Update AdivinaNumero.java

--- retos/entregas/PabloArce/AdivinaNumero.java
+++ retos/entregas/PabloArce/AdivinaNumero.java
@@ -2,30 +2,32 @@ import java.util.Scanner;
 public class AdivinaNumero{
     public static void main(String[] args) {
         Scanner entry=new Scanner(System.in);
-        int NumeroAleatorio, Intento, ContadorCaliente=0,Trampa;
+        int numeroAleatorio=0, intento=0, contadorCaliente=0,trampa=0;
         NumeroAleatorio=(int)(Math.random()*100+1);
-        Trampa=(int)(Math.random()*5+1);
-        if (Math.random()<=0.5){Trampa-=(2*Trampa);}
+        trampa=(int)(Math.random()*5+1);
+        if (Math.random()<=0.5){trampa-=(2*trampa);}
         for (int i=0;i<5;i++){
-            System.out.print("Dime un número del uno al 100: "); Intento=entry.nextInt();
+            System.out.print("Dime un número del uno al 100: "); intento=entry.nextInt();
             // Es menor, mayor o has acertado
-            if (Intento<NumeroAleatorio){System.out.println("Es mayor.");}
-            else if (NumeroAleatorio<Intento){System.out.println("Es menor.");}
+            if (intento<numeroAleatorio){System.out.println("Es mayor.");}
+            else if (numeroAleatorio<intento){System.out.println("Es menor.");}
             else{System.out.println("Has acertado en el intento número "+(i+1)+".");break;} 
             //
             ////Caliente, frio o tibio
-            int Aproximacion=NumeroAleatorio-Intento;
-            if (Aproximacion<=5 & Aproximacion>=-5){System.out.println("Caliente.");
+            int aproximacion=numeroAleatorio-intento;
+            if (aproximacion<=5 & aproximacion>=-5){System.out.println("Caliente.");
                 //Trampas
-                ContadorCaliente++;
-                if (ContadorCaliente==2){System.out.println("¡Achús!"); NumeroAleatorio+=Trampa; ContadorCaliente=0;}
-                    if (NumeroAleatorio>100){NumeroAleatorio-=(NumeroAleatorio-100);}
+                contadorCaliente++;
+                if (contadorCaliente==2){System.out.println("¡Achús!"); 
+                    numeroAleatorio+=trampa;
+                    contadorCaliente=0;}
+                    if (numeroAleatorio>100){numeroAleatorio-=(numeroAleatorio-100);}
                 //
             }
-            else if (Aproximacion<=10 & Aproximacion>=-10){System.out.println("Tibio.");ContadorCaliente=0;}
-            else{System.out.println("Frio.");ContadorCaliente=0;}
+            else if (aproximacion<=10 & aproximacion>=-10){System.out.println("Tibio.");contadorCaliente=0;}
+            else{System.out.println("Frio.");contadorCaliente=0;}
             ////
-            if (i==4){System.out.println("No te quedan mas intentos, has perdido. Era el: "+NumeroAleatorio);}
+            if (i==4){System.out.println("No te quedan mas intentos, has perdido. Era el: "+numeroAleatorio);}
         }
     }   
 }
