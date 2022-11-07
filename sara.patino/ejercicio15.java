1089c26d17cac6ba2f94dbe9513e8cab95dd25e9
sara-patino <100871932+sara-patino@users.noreply.github.com>
2022-11-07 20:38:30 +0100

    Add files via upload

---
+++ sara.patino/ejercicio15.java
@@ -0,0 +1,19 @@
+public class ejercicio15{
+    public static void main(String[] args){
+
+        for(int i=1; i<=21; i=i+1){
+            for(int j=1;j<=21;j=j+1){
+                if(j>1 && j<6 || i>1 && i<6){
+                System.out.print("[*]");
+                }else {
+                    System.out.print("[ ]");
+                
+                }
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
