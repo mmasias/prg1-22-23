666bb1e18bf709625f06a3ae729b2f3d1ae6d157
Álvaro Lostal <114483337+lostal@users.noreply.github.com>
2022-11-07 18:35:33 +0100

    Entrega 004-RetoForIf

---
+++ retos/entregas/alvaroLostal/RetoForIf.java
@@ -0,0 +1,403 @@
+public class RetoForIf {
+    public static void main(String[] args) {
+        int dimension;
+        dimension = 21;
+
+        System.out.println("Ejercicio 1");
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    i==2 && j==2
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("");
+        System.out.println("Ejercicio 2");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    i==2
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("");
+        System.out.println("Ejercicio 3");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    j==20
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("");
+        System.out.println("Ejercicio 4");
+        
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    8<=i && 14>=i
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 5");
+        
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    i==11 && j==11
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 6");
+        
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    i==11 || j==11
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 7");
+        
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    j==i
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 8");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    j==(22-i)
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 9");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    j==i || j==(22-i)
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 10");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    i<22
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 11");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    j != 11 || i!= 11
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 12");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    j!=11 && i!=11
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 13");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    i==j-1 || i==j+1
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 14");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    i==2 || i==20 || j==2 || j==20
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 15");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    (i>=2 && i<=5) || (j>=2 && j<=5)
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 16");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    (i!=1 && i!=21) && (j!=1 && j!=21)
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 17");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    (j==2 || j==20) && (i!=1 && i!=21) || (i==2 || i== 20)  && (j!=1 && j!=21)
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 18");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    i<=5 && j<=5
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 19");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    (j%2 == 1) && (i%2 == 1) || (j%2 == 0) && (i%2 == 0)
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 20");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    (i==5-j) || (i== 9-j) || (i==13-j) || (i==17-j) || (i==21-j) || (i==25-j) || (i==29-j) || (i==33-j) || (i==37-j) || (i==41-j) || (i==45-j)
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Ejercicio 21");
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    (j!=6 || i!=11) && (j!=7 || i!=9 && i!=13) && (j!=7 || i!=10 && i!=12) && (j!=8 || i!=8 && i!=14) && (j!=9 || i!=7 && i!=15) && (j!=9 || i!=7 && i!=15) && (j!=10 || i!=7 && i!=15) && (j!=11 || i!=6 && i!=16) && (j!=12 || i!=7 && i!=15) && (j!=13 || i!=7 && i!=15) && (j!=14 || i!=8 && i!=14) && (j!=15 || i!=9 && i!=13) && (j!=15 || i!=10 && i!=12) && (j!=16 || i!=11)
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
