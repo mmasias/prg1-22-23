902d3ce042325f914c058374feead1e440ec21ca
Ibuprofeno Fernández <99534750+ibuprofenofernandez@users.noreply.github.com>
2022-11-08 18:35:38 +0100

    Práctica no recomendada
    Sobretodo si tenemos un VisualStudioCode o similar en local...

--- retos/entregas/fernandezIbuprofeno/HolaMundo.java
+++ retos/entregas/fernandezIbuprofeno/HolaMundo.java
@@ -2,5 +2,6 @@ class HolaMundo{
     public static void main(String[] args){
         System.out.println("Hola mundo");
         System.out.println("Es un tutorial...");
+        System.out.println("Línea ingresada en web (NO SE RECOMIENDA!)");
     }
 }
