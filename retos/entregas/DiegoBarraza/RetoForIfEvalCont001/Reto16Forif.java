51c02a2685ed96d673bcfec8ac7ac2524a30c03f
Diegoza04 <114479536+Diegoza04@users.noreply.github.com>
2022-11-08 13:39:24 -0600

    Create Reto16Forif.java

---
+++ retos/entregas/DiegoBarraza/RetoForIfEvalCont001/Reto16Forif.java
@@ -0,0 +1,20 @@
+public class reto16Forif {
+    public static void main(String[]args){
+        int dimension;
+        dimension=21;
+
+        for(int j=1;j<=dimension;j=j+1){
+            for(int i=1;i<=dimension;i=i+1){
+
+                if (
+                    ( i >= 2 && i <= 20 ) && ( j >= 2 && j <= 20 ) 
+                ){
+                    System.out.print("(*)");
+                } else{
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+   }
+}
