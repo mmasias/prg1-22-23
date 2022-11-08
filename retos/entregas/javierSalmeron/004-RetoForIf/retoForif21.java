4a29dbedf74fd7b5d6ec7ba1c92fdc7b4aaa40a2
0xJVR <114479588+0xJVR@users.noreply.github.com>
2022-11-08 12:58:04 +0100

    Separar todo en diferentes archivos

---
+++ retos/entregas/javierSalmeron/004-RetoForIf/retoForif21.java
@@ -0,0 +1,31 @@
+// Javier Salmeron
+// Sol of https://docs.google.com/spreadsheets/d/1r7Sn8z3HDpUO8PPNc_eL3pTRCEHZ1gl8ybdL6D6aSLw/
+
+
+public class retoForif21 {
+
+    public static void main(String[] args) {
+        int dimension;
+        dimension = 21;
+
+        for (int j = 1; j <= dimension; j = j + 1) { 
+            for (int i = 1; i <= dimension; i = i + 1) { 
+
+                if (
+                // El código desde aqui
+
+                // 21
+                ((j-11)*(j-11)+(i-11)*(i-11) <= 5*5) && !((j-11)*(j-11)+(i-11)*(i-11) <= 4*4) // Calculate the points of the circle, then inverse the inside of the circle with a smaller circle
+                && !((i==8 && j==7) || (i==7 && j==8) || (i==14 && j==7) || (i==7 && j==14) || (i==15 && j==8) || (i==8 && j==15) || (i==15 && j==8) || (i==8 && j==15) || (i==14 && j==15) || (i==15 && j==14))
+
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
