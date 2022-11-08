3a641d2c2d83c460ed63a66c3a130f7c9e20999e
JoseSalceda <114105383+JoseSalceda@users.noreply.github.com>
2022-11-08 18:00:35 +0100

    Add files via upload

---
+++ retos/entregas/JoseSalceda/Reto For-If/RetoForIf14.java
@@ -0,0 +1,22 @@
+public class RetoForIf14 {
+    public static void main(String[] args){
+        int dimension;
+        dimension = 21; 
+        System.out.println("\n");
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                    i==2 || j==2 || i==20 || j==20
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
