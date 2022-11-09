f4ac791e7cfe2717048a6e52120d975db7d18640
Dani Puente <114569957+Daaniii23@users.noreply.github.com>
2022-11-09 10:44:24 +0100

    Add files via upload

---
+++ retos/entregas/danielPuente/RetoForIf/RetoForIf21.java
@@ -0,0 +1,28 @@
+public class RetoForIf21 {
+    public static void main(String[] args) {
+        int dimension;
+        dimension = 21;
+
+        //1
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+                // El código desde aqui
+                !(i==11 && (j==6||j==16))&&
+                !((j==7|| j==15) && (i>=9 && i!=11 && i<=13))&&
+                !((j==8 || j==14) && (i==8 || i==14))&&
+                !(j>=9 && j!=11 && j<=13 && (i==7 || i==15))&&
+                !(j==11 && (i==6 || i==16))
+
+                // El código hasta aquí
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
