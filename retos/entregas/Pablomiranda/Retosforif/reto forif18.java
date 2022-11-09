1f42de2ebb070e3287d8c5742935a320c38b6fca
pablomiraandaa <114480602+pablomiraandaa@users.noreply.github.com>
2022-11-09 23:30:58 +0100

    Add files via upload

---
+++ retos/entregas/Pablomiranda/Retosforif/reto forif18.java
@@ -0,0 +1,22 @@
+public class RetoForIf18 {
+
+    public static void main(String[] args) {
+        int dimension;
+        dimension = 21;
+
+        for (int i = 1; i <= dimension; i = i + 1) {
+            for (int j = 1; j <= dimension; j = j + 1) {
+
+                if (i <= 5 && j <= 5) {
+                    System.out.print("(*)");
+
+                } else {
+                    System.out.print(" . ");
+                }
+
+            }
+            System.out.println();
+
+        }
+    }
+}
---
+++ retos/entregas/Pablomiranda/Retosforif/reto forif7.java
@@ -0,0 +1,22 @@
+class RetoForIf7 {
+
+    public static void main(String[] args) {
+        int dimension;
+        dimension = 21;
+
+        for (int i = 1; i <= dimension; i = i + 1) {
+            for (int j = 1; j <= dimension; j = j + 1) {
+
+                if (i == j) {
+                    System.out.print("(*)");
+
+                } else {
+                    System.out.print(" . ");
+                }
+
+            }
+            System.out.println();
+
+        }
+    }
+}
