d286bb86d7b7fdd534abedd5c6ded417d2d55dbe
RicardoDomingosAlfredo <97975828+RicardoDomingosAlfredo@users.noreply.github.com>
2022-11-09 13:44:51 +0100

    Create Reto10.java

---
+++ retos/entregas/RicardoDomingosAlfredo/Reto10.java
@@ -0,0 +1,23 @@
+
+public class Reto1 {
+    public static void main(String[] args) {
+        int dimension;
+        dimension = 21;
+
+        for (int j = 1; j <= dimension; j = j + 1) {
+            for (int i = 1; i <= dimension; i = i + 1) {
+
+                if (
+               
+                    i==i && j==j
+
+                ) {
+                    System.out.print("(*)");
+                } else {
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
