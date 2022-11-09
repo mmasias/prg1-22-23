30de1ef854af457f16112ded045e42fac74330d4
mmasias@AIO <manuel@masiasweb.com>
2022-11-09 15:24:39 +0100

    Limpieza de código (menos líneas, menos líneas)

--- temario/EstructurasControl.java
+++ temario/EstructurasControl.java
@@ -9,27 +9,22 @@ public class EstructurasControl {
 		
 		int intentoDelUsuario = 0;
 		int numeroDeIntentos = 0;
-		int MAXIMO_DE_INTENTOS = 5;
+		final int MAXIMO_DE_INTENTOS = 5;
 		
 		boolean haAdivinado = false;
 		boolean quedanIntentos = true;
 		
 		do {
 			numeroDeIntentos = numeroDeIntentos + 1;
-			System.out.println("Intento numero " + numeroDeIntentos	 + ". Adivina [" + numeroPorAdivinar + "]: ");
+			System.out.println(numeroDeIntentos	 + "º intento... Adivina [" + numeroPorAdivinar + "]: ");
 			intentoDelUsuario = entrada.nextInt();
-			
 			if (intentoDelUsuario == numeroPorAdivinar) {
 				System.out.println("Has adivinado!");
 				haAdivinado = true;
 			}	
-			
-			if (numeroDeIntentos < MAXIMO_DE_INTENTOS) {
-				quedanIntentos = true;
-			} else {
+			if (numeroDeIntentos >= MAXIMO_DE_INTENTOS) {
 				quedanIntentos = false;
-			}
-			
+			}		
 		} while(!haAdivinado && quedanIntentos);
     }
  }
