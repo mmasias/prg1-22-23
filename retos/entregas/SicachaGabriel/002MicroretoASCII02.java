6d7f518175a2f108481505599efb334c205e0084
Gbrl413 <66096194+Gbrl413@users.noreply.github.com>
2022-11-15 12:42:38 +0100

    Update and rename Microreto2.java to 002MicroretoASCII02.java

--- retos/entregas/SicachaGabriel/002MicroretoASCII02.java
+++ retos/entregas/SicachaGabriel/002MicroretoASCII02.java
@@ -1,4 +1,4 @@
-public class Microreto2{
+public class MicroretoA02{
     public static void main(String arg[]){
        
         // for (int i = 0; i < 6; i++) { if (i==2 || i==3) { System.out.println(" ** **  "); } else { System.out.println("** ** **"); }
