a12955ccf8b33bb3440205628def19b2dc5bbfc0
AndrextA <andrextalarcon@gmail.com>
2022-11-15 14:55:44 +0100

    Update

--- retos/entregas/andrésAlarcón/CCCF/src/CCCF.java
+++ retos/entregas/andrésAlarcón/CCCF/src/CCCF.java
@@ -2,9 +2,13 @@ public class CCCF {
 	public static void main(String[] args) throws Exception {
 
 		int elapsedTime = 12 * 60, minutesWithoutCostumers = 0, costumersInQueue = 0, attendedCostumers = 0, itemsSold = 0; 
-		int checkoutOne = 0, checkoutTwo = 0, checkoutThree = 0, checkoutFour = 0, checkoutFive = 0, checkoutSix = 0;
+		int checkoutOne = 0, checkoutTwo = 0, checkoutThree = 0, checkoutFour = 0, checkoutExtra = 0;
 
 		double newCostumerProbability;
+
+		String isExtraCheckoutOpen = "Closed";
+
+		boolean checkoutExtraOpen = false;
 		
 
 		for (int minute = 1; minute <= elapsedTime; minute = minute + 1) {
@@ -17,19 +21,18 @@ public class CCCF {
 				if (newCostumerProbability < 0.4) {
 					System.out.print(" A costumer arrives -");
 					costumersInQueue = costumersInQueue + 1;
+					if (costumersInQueue > 15 && !checkoutExtraOpen) {
+						checkoutExtraOpen = true;
+						isExtraCheckoutOpen = "Open";
+
+					} else {
+						
+					}
+
 					} else {
 					System.out.print(" No one arrives -");
 					minutesWithoutCostumers = minutesWithoutCostumers + 1;
 					}
-
-				System.out.println(" In Queue: " + costumersInQueue);
-				System.out.print(" Checkout 1 [" + checkoutOne + "] |");
-				System.out.print(" Checkout 2 [" + checkoutTwo + "] |");
-				System.out.println(" Checkout 3 [" + checkoutThree + "]");
-				System.out.print(" Checkout 4 [" + checkoutFour + "] |");
-				System.out.print(" Checkout 5 [" + checkoutFive + "] |");
-				System.out.println(" Checkout 6 [" + checkoutSix + "]");
-				System.out.println("...................................................");
 
 			// Checkout costumers
 
@@ -59,18 +62,13 @@ public class CCCF {
 
 				}
 
-				if (checkoutFive <= 0 && costumersInQueue>0) {
+				if (checkoutExtra <= 0 && checkoutExtraOpen) {
 					costumersInQueue = costumersInQueue - 1;
-					attendedCostumers = attendedCostumers + 1;
-					checkoutFive = (int) (Math.random()*((10-5)+1))+5;
+                    attendedCostumers = attendedCostumers + 1;
+                    checkoutExtra = (int) (Math.random()*((10-5)+1))+5;
 				}
 
-				if (checkoutSix <= 0 && costumersInQueue>0) {
-					costumersInQueue = costumersInQueue - 1;
-					attendedCostumers = attendedCostumers + 1;
-					checkoutSix = (int) (Math.random()*((10-5)+1))+5;
-
-				}
+				
 
 
 			// Checkout items
@@ -95,27 +93,33 @@ public class CCCF {
 					itemsSold = itemsSold + 1;
 				}
 
-				if (checkoutFive > 0) {
-					checkoutFive = checkoutFive - 1;
-					itemsSold = itemsSold + 1;
+				if (checkoutExtra > 0) {
+					checkoutExtra = checkoutExtra - 1;
+                    itemsSold = itemsSold + 1;
 				}
 
-				if (checkoutSix > 0) {
-					checkoutSix = checkoutSix - 1;
-					itemsSold = itemsSold + 1;
-				}
+
+			// Checkout Display	
+
+			System.out.println(" In Queue: " + costumersInQueue);
+			System.out.print(" Checkout 1 [" + checkoutOne + "] |");
+			System.out.print(" Checkout 2 [" + checkoutTwo + "] |");
+			System.out.print(" Checkout 3 [" + checkoutThree + "] |");
+			System.out.print(" Checkout 4 [" + checkoutFour + "] |");
+			System.out.println(" Extra Checkout [" + isExtraCheckoutOpen + "] [" + checkoutExtra + "]");
+			System.out.println("..................................................................................................");
 
 		}
 
-		// Summary display
+		// Summary Display
 
 		System.out.println("════════════════════════════════════════════════════");
 		System.out.println("                 Summary of the day                 ");
 		System.out.println("════════════════════════════════════════════════════");
-		System.out.println("Minutes with empty queue: " + minutesWithoutCostumers);
-		System.out.println("Costumers in queue at closing: " + costumersInQueue);
-		System.out.println("Served Costumers: " + attendedCostumers);
-		System.out.println("Items sold during the day: " + itemsSold);
+		System.out.println(" Minutes with empty queue: " + minutesWithoutCostumers);
+		System.out.println(" Costumers in queue at closing: " + costumersInQueue);
+		System.out.println(" Served Costumers: " + attendedCostumers);
+		System.out.println(" Items sold during the day: " + itemsSold);
 		System.out.println("════════════════════════════════════════════════════");
 
 
