d2e7aae92d317e1853842f19ffc1adaa2489053a
Gbrl413 <66096194+Gbrl413@users.noreply.github.com>
2022-11-15 13:01:51 +0100

    Update 003MicroretoASCII03.java

--- retos/entregas/SicachaGabriel/003MicroretoASCII03.java
+++ retos/entregas/SicachaGabriel/003MicroretoASCII03.java
@@ -1,4 +1,4 @@
-public class Microreto03 {
+public class MicroretoA03 {
     public static void main(String args[]){
         System.out.println("+--------------------+");
         System.out.println("|**  **  **  **  **  |");
