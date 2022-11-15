f6f5f87a10c1097fcf3177bf13fcd3e32e043f38
Gbrl413 <66096194+Gbrl413@users.noreply.github.com>
2022-11-15 12:34:51 +0100

    Update and rename Microreto.java to 001MicroretoASCII01.java

--- retos/entregas/SicachaGabriel/Microreto.java
+++
@@ -1,8 +0,0 @@
-public class Microreto{
-    public static void main(String args[]){
-        for (int i = 0; i < 5; i++){
-            System.out.println("**********");
-        }
-        
-    }
-}
