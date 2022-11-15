ef87f697ca77bad2b5ecd0da4db2ac6de45dd567
Gbrl413 <66096194+Gbrl413@users.noreply.github.com>
2022-11-15 12:07:20 +0100

    Add files via upload

---
+++ retos/entregas/SicachaGabriel/Microreto2.java
@@ -0,0 +1,14 @@
+public class Microreto2{
+    public static void main(String arg[]){
+       
+        for (int i = 0; i < 6; i++) {
+            if (i==2 || i==3) {
+                System.out.println(" ** **  ");
+                
+            } else {
+                System.out.println("** ** **");
+            }
+        }
+      
+    }
+}
