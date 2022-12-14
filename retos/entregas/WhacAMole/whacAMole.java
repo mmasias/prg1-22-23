08d377240f413e8c8d8c7f52e2a120a8f63694bd
RaulPlayo <114569807+RaulPlayo@users.noreply.github.com>
2022-12-14 12:46:40 +0100

    Update whacAMole.java

--- retos/entregas/WhacAMole/whacAMole.java
+++ retos/entregas/WhacAMole/whacAMole.java
@@ -1,46 +1,47 @@
 import java.util.Scanner;
 
-public class retoWhacAMole {
-     public static void main(String[] args) {
+public class whacAMole {
+    public static void main(String[]args){
         int dimension = 4;
-        int turno = 1
-        int aciertos = 0;
+        int turno = 1;
+        int acierto = 0;
         int casilla = 0;
-        int monigote1 = 0, monigote2=0;
+        int monigote = 0, monigote1=0;
         int contador = 1;
+        
 
-        Scanner sc = new Scanner(System.in);
+        Scanner entrada =  new Scanner(System.in);
 
         do {
             contador = 1;
 
-            monigote1 = ((int) (Math.random() * 100) % 16) + 1;
+            monigote = ((int) (Math.random() * 100) % 16) + 1;
             do {
-                monigote2 = ((int) (Math.random() * 100) % 16) + 1;
-            }while (monigote1==monigote2);
+                monigote1 = ((int) (Math.random() * 100) % 16) + 1;
+            }while (monigote==monigote1);
             do {
-                System.out.println("Introzca la casilla a la que quieres probar a golpear");
-                casilla = sc.nextInt();
+                System.out.println("Introzca la casilla que desea golpear");
+                casilla = entrada.nextInt();
             } while (casilla < 0 || casilla > 16);
             for (int j = 1; j <= dimension; j = j + 1) {
                 for (int i = 1; i <= dimension; i = i + 1) {
 
-                    if (casilla == monigote1 && casilla == contador) {
+                    if (casilla == monigote && casilla == contador) {
 
                         System.out.print(" [**] ");
-                        aciertos++;
+                        acierto++;
 
 
-                    } else if (casilla == monigote2 && casilla == contador) {
+                    } else if (casilla == monigote1 && casilla == contador) {
 
                         System.out.print(" [**] ");
-                        aciertos++;
+                        acierto++;
 
 
                     } else if (contador == casilla) {
 
                         System.out.print(" [[]] ");
-                    } else if (contador == monigote1 || contador==monigote2) {
+                    } else if (contador == monigote || contador==monigote1) {
                         System.out.print(" [''] ");
                     } else {
                         System.out.print(" (  ) ");
@@ -53,11 +54,10 @@ public class retoWhacAMole {
                 System.out.println();
 
             }
-            System.out.println("Turno: " + turno + "  /  Aciertos: " + aciertos);
+            System.out.println("Turno: " + turno + "  /  Aciertos: " + acierto);
 
             turno++;
         } while (turno <= 16);
-        System.out.println("El juego ha terminado. Has tenido: " + aciertos + " aciertos");
+        System.out.println("El juego ha terminado. Has tenido: " + acierto + " aciertos");
     }
-
 }
