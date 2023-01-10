5b909230fe13f7109b1f5a00349ad95a5b3fba26
mmasias@AIO <manuel@masiasweb.com>
2023-01-10 16:57:34 +0000

    ++verbose

--- temario/codigo/MarcoMetodos.java
+++ temario/codigo/MarcoMetodos.java
@@ -8,6 +8,7 @@ class MarcoMetodos {
 
         do {
             dia++;
+            System.out.println("DIA " + dia);
             moverMarco(marco);
             moverMadre(madre);
         } while (!estenJuntos(marco, madre));
@@ -15,6 +16,8 @@ class MarcoMetodos {
     }
 
     static void moverMadre(int[] madre) {
+
+        System.out.println("> La madre avanza 80 Km");
         madre[0] = madre[0] + 80;
     }
 
@@ -24,6 +27,8 @@ class MarcoMetodos {
         double tiempo = (Math.random()*2)+8 - factorMonoEscapado();
         double espacio = velocidad * tiempo;
 
+        System.out.println("> Marco avanza " + (int) espacio);
+
         marco[0] = marco[0] + (int) espacio;
     }
 
@@ -32,17 +37,20 @@ class MarcoMetodos {
         double elClima = Math.random();
 
         if (elClima<0.1) {
+            System.out.println("> Llueve mucho!");
             return 0.25;
         } else if (elClima<0.4) {
+            System.out.println("> Llueve un poco");
             return 0.75;
         }
-
+        System.out.println("> Hace bueno");
         return 1;
     }
 
     static double factorMonoCansado(){
 
         if (Math.random()<0.25) {
+            System.out.println("> El mono se canso");
             return 0.9;
         }
         return 1;
@@ -51,6 +59,7 @@ class MarcoMetodos {
     static double factorMonoEscapado(){
         
         if (Math.random()<0.15) {
+            System.out.println("> El mono se escapo");
             return 2;
         }
         return 0;        
@@ -59,8 +68,10 @@ class MarcoMetodos {
     static boolean estenJuntos(int[] marco, int[] madre) {
 
         if(madre[0] - marco[0] < 0) {
+            System.out.println("¡Marco ha encontrado a su madre!");
             return true;
         }
+        System.out.println("> LA DISTANCIA ES " + (madre[0] - marco[0]));
         return false;
     }
 }
